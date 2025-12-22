5236494faeafa280ae54d148bf54980ec8d6c887
GiorgosKor16 <test@example.com>
2025-12-22 17:40:21 +0200

    fix: Add localhost:3001 to CORS allowed origins

--- backend/main.py
+++ backend/main.py
@@ -30,7 +30,8 @@ app = FastAPI(
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:8000", "*"],
+    allow_origins=["http://localhost:3000",
+                   "http://localhost:3001", "http://localhost:8000", "*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
