0f839d4ae58964b3f61396bf3e239c13981cd619
GiorgosKor16 <test@example.com>
2026-02-03 16:41:36 +0200

    feat: import BonusWizard component in SimpleBonusForm

--- src/components/SimpleBonusForm.tsx
+++ src/components/SimpleBonusForm.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import React from 'react';
 import { API_ENDPOINTS } from '@/lib/api-config';
+import BonusWizard from './BonusWizard';
 
 const CURRENCIES = ['EUR', 'USD', 'CAD', 'AUD', 'BRL', 'NOK', 'NZD', 'CLP', 'MXN', 'GBP', 'PLN', 'PEN', 'ZAR', 'CHF', 'NGN', 'JPY', 'AZN', 'TRY', 'KZT', 'RUB', 'UZS'];
 
